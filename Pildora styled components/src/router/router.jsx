d87bddc98d395a75dab12c979638bc3c718e425d
TrianaSolerMartin <triana287@gmail.com>
2024-02-08 09:24:42 +0100

    adding styled-component practica

---
+++ Pildora styled components/src/router/router.jsx
@@ -0,0 +1,11 @@
+import { createBrowserRouter } from "react-router-dom";
+import Button from "../components/Button";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Button/>,
+    }
+])
+
+export default router;
