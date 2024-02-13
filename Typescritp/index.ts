4e8e4fa55e296385d6b6ce886c589de744f78ffa
TrianaSolerMartin <triana287@gmail.com>
2024-02-13 08:57:56 +0100

    first-typescript

---
+++ Typescritp/index.ts
@@ -0,0 +1,29 @@
+//number, string, boolean 
+
+let myName: string = "Triana";
+
+let age: number =27;
+
+let femCoder: boolean = true;
+
+//Typescript  nos permite que uando no le especificado 
+// myName = 6;
+
+// femCoder = "Triana" ;
+
+//Any 
+
+let recipe: any = "Tacos";
+
+recipe = 4
+
+recipe = true 
+
+//union
+
+let year: number | string | boolean;
+
+year = false;
+year = 2024;
+year = "dosmilventicuatro";
+
