94677e5bc3758f18748debf90e870714443d2331
TrianaSolerMartin <triana287@gmail.com>
2024-03-01 09:51:11 +0100

    trying t follow victors lesson

---
+++ typescript/Typescript-inferencia-de-datos/main.ts
@@ -0,0 +1,24 @@
+//INFERENCIA DE DATOS
+
+let texto = "hola mundo";
+
+texto.mach(1);
+
+
+// inferencia de tipos sin necesidad de decirles el tipo e dato inferido y saber que tipo es.
+
+const PI = 3.14;
+
+const a =3;
+
+let b = a + PI;
+
+//funttions 
+
+function saludar (name:string) {
+    //name string
+    //typoany de  forma
+
+    saludar (pepe)
+}
+
